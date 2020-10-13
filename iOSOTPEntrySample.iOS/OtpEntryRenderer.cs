6eba86fa4ed942fb4a70dd8598f60ba813874268
Gerald Versluis <gerald@verslu.is>
2020-10-13 13:34:58 +0200

    Initial commit

---
+++ iOSOTPEntrySample.iOS/OtpEntryRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using iOSOTPEntrySample;
+using iOSOTPEntrySample.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(OtpEntry), typeof(OtpEntryRenderer))]
+namespace iOSOTPEntrySample.iOS
+{
+    public class OtpEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(12, 0) && Control != null)
+            {
+                Control.TextContentType = UITextContentType.OneTimeCode;
+            }
+        }
+    }
+}
---
+++ iOSOTPEntrySample/OtpEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace iOSOTPEntrySample
+{
+    public class OtpEntry : Entry
+    {
+        public OtpEntry()
+        {
+        }
+    }
+}
